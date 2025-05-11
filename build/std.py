fac1a56bc99d25032505631be74e7ff26231d03c
AnonymousPC <shyeyian@petalmail.com>
2025-05-11 16:22:20 +0800

    add submodule

--- build/std.py
+++ build/std.py
@@ -4,14 +4,14 @@ import os
 
 if __name__ == "__main__":
     if compiler == "g++":
-        if os.path.isfile(f"module/std.cppm"):
-            run(f"g++ -std=c++26 -O3 -fmodules -fmodule-only module/std.cppm -c")
+        if os.path.isfile(f"src/std_g++.cppm"):
+            run(f"g++ -std=c++26 -O3 -fmodules -fmodule-only src/std_g++.cppm -c")
         else:
-            print(f"{red}e.g.: cp .../c++/15.1.0/bits/std.cc module/std.cppm")
+            print(f"{red}e.g.: cp .../c++/15.1.0/bits/std.cc src/std_g++.cppm")
 
     elif compiler == "clang++":
-        if os.path.isdir(f"module/std") and os.path.isfile(f"module/std.cppm"):
-            run(f"clang++ -std=c++26 -O3 --precompile module/std.cppm           -c -o module/pcm.cache/std.pcm")
-            run(f"clang++ -std=c++26 -O3              module/pcm.cache/std.pcm  -c -o module/pcm.cache/std.o"  )
+        if os.path.isdir(f"src/std") and os.path.isfile(f"src/std_clang++.cppm"):
+            run(f"clang++ -std=c++26 -O3 --precompile src/std_clang++.cppm -c -o pcm.cache/std.pcm")
+            run(f"clang++ -std=c++26 -O3              pcm.cache/std.pcm    -c -o pcm.cache/std.o"  )
         else:
-            print(f"{red}e.g.: cp -r .../llvm/share/libc++/v1/std module/std && cp .../llvm/share/libc++/v1/std.cppm module/std.cppm{white}")
+            print(f"{red}e.g.: cp -r .../llvm/share/libc++/v1/std src/std && cp .../llvm/share/libc++/v1/std.cppm src/std_clang++.cppm{white}")
